ed3648dab63806d4e19cea2a4cbd6e6aaac3af3d
Akiel <122918848+Akieldamas@users.noreply.github.com>
2025-03-11 16:24:25 +0100

    STUFF

---
+++ Controller/AIController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace ArduinoMAZE.Controller
+{
+    public class AIController
+    {
+        string[,] GLOBALmazeMatrix;
+        
+        public AIController()
+        {}
+        
+        public void AILogic(string[,] mazeMatrix, int[] playerLocation) 
+        {
+            GLOBALmazeMatrix = mazeMatrix;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using ArduinoMAZE.Controller;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,27 +17,201 @@ namespace ArduinoMAZE
     /// </summary>
     public partial class MainWindow : Window
     {
+        ManualController manualController;
+
         List<string> Options = new List<string>(
             new string[] { "Manuel", "Aléatoire", "IA" }
         );
+
+        string[,] mazeMatrix =
+        {
+            { "#", "#", "#", "#", "#", "#", "#", "#", "#", "#" },
+            { "#", "P", ".", ".", ".", ".", ".", ".", ".", "#" },
+            { "#", ".", "#", "#", ".", "#", "#", "#", ".", "#" },
+            { "#", ".", "#", ".", ".", ".", ".", "#", ".", "#" },
+            { "#", ".", ".", ".", "G", "#", ".", "#", ".", "#" },
+            { "#", ".", "#", ".", "#", "#", ".", "#", ".", "#" },
+            { "#", ".", "#", ".", ".", ".", ".", "#", ".", "#" },
+            { "#", ".", "#", "#", "#", "#", "#", "#", ".", "#" },
+            { "#", ".", ".", ".", ".", ".", ".", ".", ".", "#" },
+            { "#", "#", "#", "#", "#", "#", "#", "#", "#", "#" },
+        };
+
+        int[] playerLocation = { 1, 1 };
+        int[] playerDirection = { 0, 0 };
+        int[] previousLocation = { 1, 1 };
+
         public MainWindow()
         {
             InitializeComponent();
-            CB_Options.Items.Add(Options);
+            manualController = new ManualController();
+            CB_Options.ItemsSource = Options;
+            Grid_Maze.Children.Clear();
         }
-
         public void Load_Model()
         {
-            // Load the model
+            BTN_Start.IsEnabled = true;
+            InitializeMaze();
         }
 
-        private void BTN_Start_Click(object sender, RoutedEventArgs e)
+        private async void BTN_Start_Click(object sender, RoutedEventArgs e)
         {
+            InitializeMaze();
+
+            if (CB_Options.SelectedItem == null)
+            {
+                MessageBox.Show("Please choose a mode");
+                return;
+            }
+
             BTN_Start.IsEnabled = false;
             BTN_Stop.IsEnabled = true;
             BTN_Reset.IsEnabled = false;
+            BTN_Load.IsEnabled = false;
+
+            string selectedOption = CB_Options.SelectedItem.ToString();
+
+            if (selectedOption == "Manuel")
+            {
+                RunManual();
+            }
+            else if (selectedOption == "Aléatoire")
+            {
+                // Aléatoire
+            }
+            else if (selectedOption == "IA")
+            {
+                AIController aiController = new AIController();
+                //  aiController.AILogic(mazeMatrix, playerRow = 1, playerCol = 1);
+            }
+            else
+            {
+                MessageBox.Show("Please choose a mode");
+                return;
+            }
+        }
+
+        bool KeyPressed = false;
+
+        private async Task RunManual()
+        {
+            int[] Goal = { 4, 4 };
+            KeyPressed = false;
+            KeyDown += new KeyEventHandler(MainWindow_KeyDown);
+            while (playerLocation != Goal)
+            {
+                await Task.Delay(250);
+                if (KeyPressed)
+                {
+                    bool Decision = manualController.ManualLogic(mazeMatrix, playerLocation, playerDirection);
+                    if (Decision)
+                    {
+                        previousLocation = new int[] { playerLocation[0], playerLocation[1] };
+                        playerLocation = new int[] { playerLocation[0] + playerDirection[0], playerLocation[1] + playerDirection[1] };
+                        // stop maze when reached goal location etc
+                        UpdateMaze(playerLocation, previousLocation);
+                    }
+                }
+                KeyPressed = false;
+
+            }
+            MessageBox.Show("Congratulations");
+
+        }
 
 
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Up:
+                    playerDirection = new int[] { -1, 0 };
+                    KeyPressed = true;
+                    break;
+                case Key.Down:
+                    playerDirection = new int[] { 1, 0 };
+                    KeyPressed = true;
+                    break;
+                case Key.Left:
+                    playerDirection = new int[] { 0, -1 };
+                    KeyPressed = true;
+                    break;
+                case Key.Right:
+                    playerDirection = new int[] { 0, 1 };
+                    KeyPressed = true;
+                    break;
+            }
+        }
+
+        public void UpdateMaze(int[] playerLocation, int[] previousLocation)
+        {
+            mazeMatrix[playerLocation[0], playerLocation[1]] = "P";
+            mazeMatrix[previousLocation[0], previousLocation[1]] = ".";
+
+            for (int row = 0; row < 10; row++)
+            {
+                for (int col = 0; col < 10; col++)
+                {
+                    Rectangle rect = new Rectangle();
+                    if (row == playerLocation[0] && col == playerLocation[1])
+                    {
+                        rect.Fill = Brushes.Blue;
+                    }
+                    if (row == previousLocation[0] && col == previousLocation[1])
+                    {
+                        rect.Fill = Brushes.White;
+                    }
+                    if (mazeMatrix[row, col] == "#")
+                    {
+                        rect.Fill = Brushes.Black;
+                    }
+                    if (mazeMatrix[row, col] == ".")
+                    {
+                        rect.Fill = Brushes.White;
+                    }
+                    if (mazeMatrix[row, col] == "P")
+                    {
+                        rect.Fill = Brushes.Blue;
+                    }
+                    if (mazeMatrix[row, col] == "G")
+                    {
+                        rect.Fill = Brushes.Red;
+                    }
+                    Grid.SetRow(rect, row);
+                    Grid.SetColumn(rect, col);
+                    Grid_Maze.Children.Add(rect);
+                }
+            }
+        }
+
+        public void InitializeMaze()
+        {
+            for (int row = 0; row < 10; row++)
+            {
+                for (int col = 0; col < 10; col++)
+                {
+                    Rectangle rect = new Rectangle();
+                    if (mazeMatrix[row, col] == "#")
+                    {
+                        rect.Fill = Brushes.Black;
+                    }
+                    if (mazeMatrix[row, col] == ".")
+                    {
+                        rect.Fill = Brushes.White;
+                    }
+                    if (mazeMatrix[row, col] == "P")
+                    {
+                        rect.Fill = Brushes.Blue;
+                    }
+                    if (mazeMatrix[row, col] == "G")
+                    {
+                        rect.Fill = Brushes.Red;
+                    }
+                    Grid.SetRow(rect, row);
+                    Grid.SetColumn(rect, col);
+                    Grid_Maze.Children.Add(rect);
+                }
+            }
         }
 
         private void BTN_Stop_Click(object sender, RoutedEventArgs e)
@@ -52,6 +227,8 @@ namespace ArduinoMAZE
             BTN_Reset.IsEnabled = false;
             BTN_Start.IsEnabled = true;
             BTN_Stop.IsEnabled = true;
+
+            InitializeMaze();
         }
 
         private void BTN_Load_Click(object sender, RoutedEventArgs e)
