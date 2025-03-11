c3207b8d30f29e774928dbedeb58beaf65423619
Akieldamas <122918848+Akieldamas@users.noreply.github.com>
2025-03-11 19:57:47 +0100

    Manual Mode

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using ArduinoMAZE.Controller;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,10 +20,25 @@ namespace ArduinoMAZE
     public partial class MainWindow : Window
     {
         ManualController manualController;
-
-        List<string> Options = new List<string>(
+        
+        //ObservableCollection est apparément mieux que List pour les bindings.
+        ObservableCollection<string> Options = new ObservableCollection<string>(
             new string[] { "Manuel", "Aléatoire", "IA" }
         );
+
+        string[,] defaultMatrix =
+        {
+            { "#", "#", "#", "#", "#", "#", "#", "#", "#", "#" },
+            { "#", "P", ".", ".", ".", ".", ".", ".", ".", "#" },
+            { "#", ".", "#", "#", ".", "#", "#", "#", ".", "#" },
+            { "#", ".", "#", ".", ".", ".", ".", "#", ".", "#" },
+            { "#", ".", ".", ".", "G", "#", ".", "#", ".", "#" },
+            { "#", ".", "#", ".", "#", "#", ".", "#", ".", "#" },
+            { "#", ".", "#", ".", ".", ".", ".", "#", ".", "#" },
+            { "#", ".", "#", "#", "#", "#", "#", "#", ".", "#" },
+            { "#", ".", ".", ".", ".", ".", ".", ".", ".", "#" },
+            { "#", "#", "#", "#", "#", "#", "#", "#", "#", "#" },
+        };
 
         string[,] mazeMatrix =
         {
@@ -40,12 +57,16 @@ namespace ArduinoMAZE
         int[] playerLocation = { 1, 1 };
         int[] playerDirection = { 0, 0 };
         int[] previousLocation = { 1, 1 };
+        int Score = 1000;
+        bool isRunning;
+        bool KeyPressed = false;
 
         public MainWindow()
         {
             InitializeComponent();
             manualController = new ManualController();
             CB_Options.ItemsSource = Options;
+            TB_Score.Text = $"Score: {Score}";
             Grid_Maze.Children.Clear();
         }
         public void Load_Model()
@@ -71,54 +92,59 @@ namespace ArduinoMAZE
 
             string selectedOption = CB_Options.SelectedItem.ToString();
 
-            if (selectedOption == "Manuel")
+            switch(selectedOption)
             {
-                RunManual();
-            }
-            else if (selectedOption == "Aléatoire")
-            {
-                // Aléatoire
-            }
-            else if (selectedOption == "IA")
-            {
-                AIController aiController = new AIController();
-                //  aiController.AILogic(mazeMatrix, playerRow = 1, playerCol = 1);
-            }
-            else
-            {
-                MessageBox.Show("Please choose a mode");
-                return;
+                case "Manuel":
+                    isRunning = true;
+                    RunManual();
+                    break;
+                case "Aléatoire":
+                    // Aléatoire
+                    break;
+                case "IA":
+                    AIController aiController = new AIController();
+                    // aiController.AILogic(mazeMatrix, playerRow = 1, playerCol = 1);
+                    break;
+                case "":
+                    MessageBox.Show("Please choose a mode");
+                    return;
+                case null:
+                    MessageBox.Show("Please choose a mode");
+                    return;
             }
         }
 
-        bool KeyPressed = false;
-
         private async Task RunManual()
         {
-            int[] Goal = { 4, 4 };
             KeyPressed = false;
             KeyDown += new KeyEventHandler(MainWindow_KeyDown);
-            while (playerLocation != Goal)
+            while (isRunning)
             {
-                await Task.Delay(250);
+                Debug.WriteLine("{ " + playerLocation[0] + " , " + playerLocation[1] + " }");
+                Debug.WriteLine("Running: "+ isRunning);
+                await Task.Delay(100);
                 if (KeyPressed)
                 {
                     bool Decision = manualController.ManualLogic(mazeMatrix, playerLocation, playerDirection);
                     if (Decision)
                     {
+                        Score -= 50;
+                        TB_Score.Text = $"Score: {Score}";
+
                         previousLocation = new int[] { playerLocation[0], playerLocation[1] };
                         playerLocation = new int[] { playerLocation[0] + playerDirection[0], playerLocation[1] + playerDirection[1] };
-                        // stop maze when reached goal location etc
+                        if (mazeMatrix[playerLocation[0], playerLocation[1]] == "G")
+                        {
+                            // Task.Run() est utilisé pour eviter que messagebox bloque le code (var task a fix le fait que ca marchait pas)
+                            var task = Task.Run(() => MessageBox.Show("You won!"));
+                            isRunning = false;
+                        }
                         UpdateMaze(playerLocation, previousLocation);
                     }
                 }
                 KeyPressed = false;
-
             }
-            MessageBox.Show("Congratulations");
-
         }
-
 
         private void MainWindow_KeyDown(object sender, KeyEventArgs e)
         {
@@ -148,38 +174,21 @@ namespace ArduinoMAZE
             mazeMatrix[playerLocation[0], playerLocation[1]] = "P";
             mazeMatrix[previousLocation[0], previousLocation[1]] = ".";
 
-            for (int row = 0; row < 10; row++)
+            foreach (UIElement child in Grid_Maze.Children)
             {
-                for (int col = 0; col < 10; col++)
+                if (child is Rectangle rect)
                 {
-                    Rectangle rect = new Rectangle();
+                    int row = Grid.GetRow(rect);
+                    int col = Grid.GetColumn(rect);
+                    
                     if (row == playerLocation[0] && col == playerLocation[1])
                     {
                         rect.Fill = Brushes.Blue;
                     }
-                    if (row == previousLocation[0] && col == previousLocation[1])
+                    else if (row == previousLocation[0] && col == previousLocation[1])
                     {
                         rect.Fill = Brushes.White;
                     }
-                    if (mazeMatrix[row, col] == "#")
-                    {
-                        rect.Fill = Brushes.Black;
-                    }
-                    if (mazeMatrix[row, col] == ".")
-                    {
-                        rect.Fill = Brushes.White;
-                    }
-                    if (mazeMatrix[row, col] == "P")
-                    {
-                        rect.Fill = Brushes.Blue;
-                    }
-                    if (mazeMatrix[row, col] == "G")
-                    {
-                        rect.Fill = Brushes.Red;
-                    }
-                    Grid.SetRow(rect, row);
-                    Grid.SetColumn(rect, col);
-                    Grid_Maze.Children.Add(rect);
                 }
             }
         }
@@ -191,35 +200,36 @@ namespace ArduinoMAZE
                 for (int col = 0; col < 10; col++)
                 {
                     Rectangle rect = new Rectangle();
-                    if (mazeMatrix[row, col] == "#")
+                    switch(mazeMatrix[row, col])
                     {
-                        rect.Fill = Brushes.Black;
+                        case "#":
+                            rect.Fill = Brushes.Black;
+                            break;
+                        case ".":
+                            rect.Fill = Brushes.White;
+                            break;
+                        case "P":
+                            rect.Fill = Brushes.Blue;
+                            break;
+                        case "G":
+                            rect.Fill = Brushes.Red;
+                            break;
                     }
-                    if (mazeMatrix[row, col] == ".")
-                    {
-                        rect.Fill = Brushes.White;
-                    }
-                    if (mazeMatrix[row, col] == "P")
-                    {
-                        rect.Fill = Brushes.Blue;
-                    }
-                    if (mazeMatrix[row, col] == "G")
-                    {
-                        rect.Fill = Brushes.Red;
-                    }
+
                     Grid.SetRow(rect, row);
                     Grid.SetColumn(rect, col);
                     Grid_Maze.Children.Add(rect);
                 }
             }
         }
-
         private void BTN_Stop_Click(object sender, RoutedEventArgs e)
         {
             BTN_Start.IsEnabled = true;
             BTN_Stop.IsEnabled = false;
             BTN_Reset.IsEnabled = true;
+            BTN_Load.IsEnabled = true;
 
+            isRunning = false;
         }
 
         private void BTN_Reset_Click(object sender, RoutedEventArgs e)
@@ -228,6 +238,16 @@ namespace ArduinoMAZE
             BTN_Start.IsEnabled = true;
             BTN_Stop.IsEnabled = true;
 
+            playerLocation = new int[] { 1, 1 };
+            playerDirection = new int[] { 0, 0 };
+            previousLocation = new int[] { 1, 1 };
+
+            Score = 1000;
+            TB_Score.Text = $"Score: {Score}";
+            mazeMatrix = (string[,])defaultMatrix.Clone();
+
+            MessageBox.Show(mazeMatrix[4,4].ToString());
+            MessageBox.Show(defaultMatrix[4,4].ToString());
             InitializeMaze();
         }
 
